5d3e8c8efcb4f2af6c7fc5d55e942ee49ffa1ab4
awlana04 <cawlana040@gmail.com>
2026-02-08 00:00:15 -0300

    🏛️ :STYLE [Section Title]: create molecule component

---
+++ src/components/molecules/section-title.tsx
@@ -0,0 +1,37 @@
+import Icon from '../atoms/icon';
+
+type SectionProps = {
+  icon: React.ElementType;
+  iconDirection: 'right' | 'left';
+  text: string;
+  color: 'lightGreen' | 'darkGreen' | 'blackDarkGreen';
+  largeWidth?: boolean;
+};
+
+export default function SectionTitle(props: SectionProps) {
+  return (
+    <div
+      className={`${props.largeWidth ? 'w-80' : 'w-64'} h-16 pl-6 flex flex-row my-2 items-center rounded-tr-2xl rounded-br-2xl ${props.color === 'lightGreen' ? 'bg-lightGreen' : 'bg-darkGreen'}`}
+    >
+      {props.iconDirection === 'left' && (
+        <Icon
+          icon={props.icon}
+          color={props.color === 'darkGreen' ? 'white' : 'black'}
+          size='large'
+        />
+      )}
+      <p
+        className={`text-2xl ml-4 ${props.color === 'darkGreen' ? 'text-white' : 'text-black'}`}
+      >
+        {props.text}
+      </p>
+      {props.iconDirection === 'right' && (
+        <Icon
+          icon={props.icon}
+          color={props.color === 'darkGreen' ? 'white' : 'black'}
+          size='large'
+        />
+      )}
+    </div>
+  );
+}
