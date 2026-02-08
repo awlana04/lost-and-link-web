ccf5337545492f0414b13569cb3624c383e34a16
awlana04 <cawlana040@gmail.com>
2026-02-07 23:58:46 -0300

    🏛️ :STYLE [Load More]: create atom component

---
+++ src/components/atoms/load-more.tsx
@@ -0,0 +1,7 @@
+export default function LoadMore() {
+  return (
+    <button className='w-24 h-8 rounded-2xl bg-darkGreen flex items-center justify-center mt-4'>
+      <p className='text-lightGreen font-medium text-sm'>Ver mais...</p>
+    </button>
+  );
+}
