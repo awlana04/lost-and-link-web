443f0fa6748fc07cdd1b11eb87017aaf0c05bc69
awlana04 <cawlana040@gmail.com>
2026-02-07 23:57:21 -0300

    🏛️ :STYLE [Card]: create atom component

---
+++ src/components/atoms/card.tsx
@@ -0,0 +1,20 @@
+type CardType = {
+  title: string;
+  description: string;
+};
+
+export default function Card(props: CardType) {
+  return (
+    <div className='w-96 h-24 rounded-2xl flex flex-row items-center bg-lightGreen pr-4 m-2'>
+      <div className='w-24 h-24 rounded-2xl bg-darkGreen' />
+      <div className='flex flex-col my-2 ml-4'>
+        <p className='text-darkGreen text-x'>{props.title}</p>
+        <p className='text-black w-64 text-lg'>
+          {props.description.length > 48
+            ? props.description.substring(0, 48) + '...'
+            : props.description}
+        </p>
+      </div>
+    </div>
+  );
+}
