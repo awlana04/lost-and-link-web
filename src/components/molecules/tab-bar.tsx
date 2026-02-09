e990d6be23994d97b2c423ec78a70574757e99d2
awlana04 <cawlana040@gmail.com>
2026-02-08 23:53:33 -0300

    🏛️ :STYLE [Tab Bar]: create molecule component

---
+++ src/components/molecules/tab-bar.tsx
@@ -0,0 +1,33 @@
+import { FiPlusCircle, FiSearch, FiSettings } from 'react-icons/fi';
+
+import Icon from '../atoms/icon';
+
+type TabBarPropsType = {
+  pageName: 'settings' | 'home' | 'add';
+};
+
+export default function TabBar(props: TabBarPropsType) {
+  return (
+    <div className='w-2xl rounded-t-2xl h-32 pb-10 bg-lightGreen flex items-center justify-center flex-row gap-32'>
+      <Icon
+        color={props.pageName === 'home' ? 'green' : 'black'}
+        size='large'
+        icon={FiSearch}
+      />
+
+      <div className='rounded-2xl w-16 h-16 flex items-center justify-center absolute -top-5 bg-darkGreen'>
+        <Icon
+          color={props.pageName === 'home' ? 'white' : 'black'}
+          size='large'
+          icon={FiPlusCircle}
+        />
+      </div>
+
+      <Icon
+        color={props.pageName === 'settings' ? 'green' : 'black'}
+        size='large'
+        icon={FiSettings}
+      />
+    </div>
+  );
+}
